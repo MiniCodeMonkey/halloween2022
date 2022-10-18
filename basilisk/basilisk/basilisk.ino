64f1336b384dae635b99fa4968551cf0d53fd433
Mathias Hansen <me@codemonkey.io>
2022-10-18 21:40:19 +0200

    Working on Basilisk firmware

--- basilisk/basilisk/basilisk.ino
+++ basilisk/basilisk/basilisk.ino
@@ -1,3 +1,12 @@
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include "wifi_credentials.h"
+
+#define DEVICE_NAME "basilisk"
+#define WIFI_CONNECT_TIMEOUT_MS 15000
+
 #define PIR_SENSOR_PIN A2
 #define BATTERY_VOLTAGE_PIN A13
 
@@ -10,16 +19,38 @@ int pirVal = 0;
 int batteryVoltage = 0;
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFI Connection Failed");
+    delay(1000);
+
+    if (millis() > WIFI_CONNECT_TIMEOUT_MS) {
+      Serial.println("Giving up. Continuing without WiFi");
+      break;
+    }
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    ArduinoOTA.setHostname(DEVICE_NAME);
+    ArduinoOTA.begin();
+  
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
   pinMode(PIR_SENSOR_PIN, INPUT);
   pinMode(BATTERY_VOLTAGE_PIN, INPUT);
   
   pinMode(SPRINKLER_RELAY_PIN, OUTPUT);
   digitalWrite(SPRINKLER_RELAY_PIN, LOW);
-
-  Serial.begin(115200);
 }
 
 void loop() {
+  ArduinoOTA.handle();
+  
   /*
   digitalWrite(SPRINKLER_RELAY_PIN, HIGH);
   delay(500);
@@ -27,15 +58,6 @@ void loop() {
   delay(10000);
   */
 
-  // Reference voltage on ESP32 is 1.1V
-  // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html#adc-calibration
-  // See also: https://bit.ly/2zFzfMT
-  int rawValue = analogRead(BATTERY_VOLTAGE_PIN);
-  float voltageLevel = (rawValue / 4095.0) * 2 * 1.1 * 3.3;
-  float batteryFraction = voltageLevel / MAX_BATTERY_VOLTAGE;
-
-  Serial.println((String)"Raw:" + rawValue + " Voltage:" + voltageLevel + "V Percent: " + (batteryFraction * 100) + "%");
-  
   pirVal = digitalRead(PIR_SENSOR_PIN);
 
   if (pirVal == HIGH) {
@@ -45,4 +67,15 @@ void loop() {
   }
 
   delay(1000);
+}
+
+void checkBattery() {
+  // Reference voltage on ESP32 is 1.1V
+  // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html#adc-calibration
+  // See also: https://bit.ly/2zFzfMT
+  int rawValue = analogRead(BATTERY_VOLTAGE_PIN);
+  float voltageLevel = (rawValue / 4095.0) * 2 * 1.1 * 3.3;
+  float batteryFraction = voltageLevel / MAX_BATTERY_VOLTAGE;
+
+  Serial.println((String)"Raw:" + rawValue + " Voltage:" + voltageLevel + "V Percent: " + (batteryFraction * 100) + "%");
 }
